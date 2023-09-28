f8d076385e8d289159ffc573f50f6bbda5ea0185
Rodrigo Oliveira <rodrigo.olv.santos@gmail.com>
2023-09-28 18:18:22 -0300

    Agora vAi

--- HelloWord/src/Application/helloWord.java
+++ HelloWord/src/Application/helloWord.java
@@ -5,7 +5,7 @@ public class helloWord {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git");
-		System.out.println("Hello Git");
+		System.out.println("Teste 2");
 	}
 
 }
